ca1dfe884ad3ea040afd68fdd25990552f1a7219
yelsharawy <yelsharawy20@stuy.edu>
2020-11-02 10:41:38 -0500

    Added constructors

--- SuperArray.java
+++ SuperArray.java
@@ -3,4 +3,13 @@ public class SuperArray {
     private String[] data;
     private int size;
 
+    public SuperArray(int capacity) {
+        data = new String[capacity];
+        size = 0;
+    }
+
+    public SuperArray() {
+        this(8);
+    }
+
 }
