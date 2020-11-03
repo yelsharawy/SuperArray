d581051ddd4ca9e493d45699addf5acd4a7473a8
yelsharawy <yelsharawy20@stuy.edu>
2020-11-03 12:30:56 -0500

    Added indexOf method

--- SuperArray.java
+++ SuperArray.java
@@ -55,6 +55,15 @@ public class SuperArray {
         return false;
     }
 
+    public int indexOf(String s) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(s)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public String remove(int index) {
         String removed = data[index];
         size--;
